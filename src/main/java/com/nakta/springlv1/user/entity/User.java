ad57a80a2b2a47b17bedfb066b1bcbbfa0263776
DESKTOP-HMN2L1J\정강용 <tuto3355@naver.com>
2023-09-02 12:48:05 +0900

    Update : 에러 처리 방식 변경
    - CustomException 으로 에러 제어

--- src/main/java/com/nakta/springlv1/user/entity/User.java
+++ src/main/java/com/nakta/springlv1/user/entity/User.java
@@ -1,8 +1,10 @@
 package com.nakta.springlv1.user.entity;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
 
 @Entity
 @Table(name = "users")
